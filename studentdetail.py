48db599bff54f8e62a43fe8458ec4b20ea5e63a7
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-10-01 15:13:00 +0530

    Add files via upload
    my first python commit

---
+++ studentdetail.py
@@ -0,0 +1,9 @@
+print("hello world")
+name=input("enter the name of the student")
+roll_no=int(input("enter the roll no of the student"))
+roll_no=roll_no+1
+print("name of the student",name)
+print("roll no of the student",roll_no)
+CGP=float(input("enter the students CGP"))
+PERCENTAGE=cgp*10
+PRINT("the student percen")
