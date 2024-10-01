6b3a589def703676f9220db7c44939a953d9421f
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-10-01 15:33:05 +0530

    student details with header statement

---
+++ studentdetail.py
@@ -0,0 +1,14 @@
+'''
+PYTHON PROGRAMME TO GET THE STUDENT DETAILS
+AUTHOR: JOSEPH
+DATE-1ST OCTOBER
+'''
+print("hello world")
+name=input("enter the name of the student")
+roll_no=int(input("enter the roll no of the student"))
+print("roll no of the student",roll_no)
+CGP=float(input("enter the students CGP"))
+PERCENTAGE=CGP*10
+print("name of the student",name)
+print("roll no of the student",roll_no)
+print("the student percentage is",PERCENTAGE)
