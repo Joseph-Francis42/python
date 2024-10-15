b98bb619efc2397deafa77e45245c1b8bff185e3
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-10-15 14:46:17 +0530

    Add files via upload

--- if program.py
+++ if program.py
@@ -1,8 +1,3 @@
-'''Author:Joseph Francis
-   Date:15-10-2024
-   program for finding greater number
-'''
-
 number1=int(input("enter a number:"))
 number2=int(input("enter a number:"))
 if number1>number2:
