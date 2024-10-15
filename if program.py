23c521bbffd4a45f5f7d67ba7d1cd4dc3eeafef9
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-10-15 14:29:13 +0530

    Add files via upload

---
+++ if program.py
@@ -0,0 +1,6 @@
+number1=int(input("enter a number:"))
+number2=int(input("enter a number:"))
+if number1>number2:
+    print(number1,"is greater than",number2)
+else:
+    print(number1,"is less than",number2)
