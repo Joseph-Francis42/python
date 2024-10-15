ecee9454644130d537f6d86ec759216c522d7615
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-10-15 14:33:19 +0530

    Update if program.py

--- if program.py
+++ if program.py
@@ -1,3 +1,8 @@
+'''Author:Joseph Francis
+   Date:15-10-2024
+   program for finding greater number
+'''
+
 number1=int(input("enter a number:"))
 number2=int(input("enter a number:"))
 if number1>number2:
