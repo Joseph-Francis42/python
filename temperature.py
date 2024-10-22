bbdff2bbaebad170e00005e6ef77e028330a2785
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2024-10-22 14:45:58 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,10 @@
+temp=int(input("Enter the temperature:"))
+a=input("Is this is (C)elsius or (F)ahrenheit?")
+if a=='C':
+    f=(9/5*temp)+32
+    print(temp,"celsius is",f,"fahrenheit")
+elif a=="F":
+    c=5/9*(temp-32)
+    print(temp,"fahrenheit is",c,"celsius")
+else:
+    print("invalid temperature")
